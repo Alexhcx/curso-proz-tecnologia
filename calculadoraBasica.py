383a34207776b354568bb3ab5a139ce640864527
Alexandre <6085319+Alexhcx@users.noreply.github.com>
2023-11-09 06:45:08 -0300

    Exercicios

--- calculadoraBasica.py
+++ calculadoraBasica.py
@@ -11,7 +11,7 @@ def calculadora (num_1, num_2, escolha):
         return 0
 
 print("o resultado da operação é",calculadora(10, 2, 1))  # soma = 12
-print("o resultado da operação é",calculadora(14, 93, 2)) # subtração = 79
+print("o resultado da operação é",calculadora(14, 93, 2)) # subtração = -79
 print("o resultado da operação é",calculadora(31, 12, 3)) # multiplicação = 372
-print("o resultado da operação é",calculadora(90, 30, 4)) # divisão = 3
+print("o resultado da operação é",calculadora(90, 30, 4)) # divisão = 3.0
 print("o resultado da operação é",calculadora(10, 2, 5))  # 0  
