738043f723c1f6dbfca923472b3b691adeba1ab1
Alexandre <6085319+Alexhcx@users.noreply.github.com>
2023-11-10 07:06:20 -0300

    Exercicios

---
+++ calculadoraInput.py
@@ -0,0 +1,53 @@
+# Faça uma função calculadora que os números e as operações serão feitas pelo usuário. O código deve ficar rodando infinitamente até que o usuário escolha a opção de sair. No início, o programa mostrará a seguinte lista de operações:
+
+# 1: Soma
+# 2: Subtração
+# 3: Multiplicação
+# 4: Divisão
+# 0: Sair
+
+# Digite o número para a operação correspondente e caso o usuário introduza qualquer outro, o sistema deve mostrar a mensagem “Essa opção não existe” e voltar ao menu de opções.
+
+# Após a seleção, o sistema deve pedir para o usuário inserir o primeiro e segundo valor, um de cada. Depois precisa executar a operação e mostrar o resultado na tela. Quando o usuário escolher a opção “Sair”, o sistema irá parar.
+
+# É necessário que o sistema mostre as opções sempre que finalizar uma operação e mostrar o resultado. 
+
+def realizarOperacao(escolha, num1, num2):
+    if escolha == 1:
+        return num1 + num2
+    elif escolha == 2:
+        return num1 - num2
+    elif escolha == 3:
+        return num1 * num2
+    elif escolha == 4:
+        return num1 / num2
+    else:
+        return "Essa opção não existe"
+
+print("Bem vindo a Calculadora, escolha a operação que deseja realizar:")
+print("1: Soma", "2: Subtração", "3: Multiplicação", "4: Divisão", "0: Sair", sep="\n")
+escolha = int(input("Digite o número: "))
+
+while escolha != 0:
+    if escolha == 1:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        print(f"O resultado da soma é: {realizarOperacao(escolha, num1, num2)}")
+    elif escolha == 2:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        print(f"O resultado da subtração é: {realizarOperacao(escolha, num1, num2)}")
+    elif escolha == 3:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        print(f"O resultado da multiplicação é: {realizarOperacao(escolha, num1, num2)}")
+    elif escolha == 4:
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+        print(f"O resultado da divisão é: {realizarOperacao(escolha, num1, num2)}")
+    else:
+        print("Essa opção não existe")
+    print("1: Soma", "2: Subtração", "3: Multiplicação", "4: Divisão", "0: Sair", sep="\n")
+    escolha = int(input("Digite o número: "))
+
+print("Obrigado por usar a calculadora!")
