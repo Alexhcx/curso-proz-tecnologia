49e1b8dc2c59aaf342373e9fa4d78a768d493838
Alexandre Nazareth <alexandrenazareth@hotmail.com>
2024-01-12 18:57:41 -0300

    Repositorio Atualizado

---
+++ JavaScript/DesenvolvimentoExtra5-1/script.js
@@ -0,0 +1,23 @@
+let elementoH1 = document.getElementById("titulo")
+elementoH1.innerText = `Desenvolvimento extra 5.1`
+
+let elementoA = document.querySelector("a")
+elementoA.innerText = `Site da Proz`
+
+let elementoUl = document.querySelector("ul")
+elementoUl.innerHTML = `
+Lista de atividades:
+<li>Crie um arquivo HTML</li>
+<li>Crie um arquivo CSS</li>
+<li>Crie um arquivo JavaScript</li>
+`
+
+let elementoOl = document.getElementById("lista-ordenada")
+elementoOl.innerHTML = `
+Lista de sites:
+<li><a href="https://www.youtube.com/" target="_blank">Youtube</a></li>
+<li><a href="https://www.google.com/" target="_blank">Google</a></li>
+<li><a href="https://flexboxfroggy.com/" target="_blank">Flexbox Froggy</a></li>
+`
+
+
